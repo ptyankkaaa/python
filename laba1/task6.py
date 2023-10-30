bafea98d83ebe1cc667311542a08800a1e9c0264
ptyankkaaa <85608729+ptyankkaaa@users.noreply.github.com>
2023-10-30 12:09:05 +0500

    Add files via upload

---
+++ laba1/task6.py
@@ -0,0 +1,24 @@
+import math
+
+i=0
+while i==0:
+    a = int(input("Enter a = "))
+    if (a < 1):
+        input("Error, a>=1 try again [Enter]")
+        i=0
+    else:
+        i+=1
+        break
+
+i=0
+while i==0:
+    b = int(input("Enter b = "))
+    if (b > 100):
+        input("Error, b<=100 try again [Enter]")
+        i=0
+    else:
+        i+=1
+        break
+
+print(pow((a*b),(1/2)))
+
