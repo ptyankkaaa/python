faf27aa9f280ce23d459ab7f432efa3fe7428224
ptyankkaaa <ptyankkaaa@yandex.ru>
2023-11-04 23:48:48 +0500

    Update task8.py
    DONE

--- laba1/task8.py
+++ laba1/task8.py
@@ -1,8 +1,9 @@
+from cmath import sqrt
 import math
 
 i=0
 while i==0:
-    A = int(input("Enter A != 0 and A >= (-10). A = "))
+    A = int(input("Enter A != 0 and A >= (-10) : A = "))
     if (A == 0 or A <(-10)):
         print("Error, try again")
         i=0
@@ -14,12 +15,23 @@ B = int(input("Enter B = "))
 
 i=0
 while i==0:
-    C = int(input("Enter C != 0 and C >= (-10). C = "))
-    if (C == 0 or C <(-10)):
+    C = int(input("Enter C <= 10 : C = "))
+    if (C > 10):
         print("Error, try again")
         i=0
     else:
         i+=1
         break
 
-def d():
+x1 = ((-B-(math.sqrt(abs(pow(B,2)-(4*A*C)))))/(2*A))
+x2 = ((-B+(math.sqrt(abs(pow(B,2)-(4*A*C)))))/(2*A))
+
+if x1<x2:
+    print("x1 =", "%.4f"%x1)
+    print("x2 =", "%.4f"%x2)
+else:
+    print("x2 =", "%.4f"%x2)
+    print("x1 =", "%.4f"%x1)
+
+
+
