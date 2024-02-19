9c28f4b042117e11f858368c590f6edf9d2bf361
ptyankkaaa <ptyankkaaa@yandex.ru>
2024-02-20 02:16:23 +0500

    Update task6.py

--- laba4/task6.py
+++ laba4/task6.py
@@ -1,13 +1,17 @@
 bork = []
 
 
+#   - Эта функция заполняет матрицу bork для четного количества столбцов. 
+#   - Генерируется двумерный список bork размером m_str x m_col, заполненный последовательными числами, начиная с 1.
+#   - Четные столбцы заполняются снизу вверх, а нечетные - сверху вниз.
+#   - Работа завершается возвращением заполненной матрицы bork.
 def cols_is_even(m_str, m_col):
     global bork
-    bork = [[0]*m_col for i in range(m_str)]  #создается двумерный список с размером `m_str` строк на `m_col` столбцов, заполненный нулями
-    count = 0 #для отслеживания номера текущего элемента в матрице
-    for col in range(m_col-1, -1, -1): #бежит по столбцам от последнего к первому
-        if col % 2 == 0: #проверяет столбец на чётность
-            for row in range(m_str-1, -1, -1): #бежит по строкам от последнего к первому
+    bork = [[0]*m_col for i in range(m_str)]
+    count = 0 
+    for col in range(m_col-1, -1, -1): 
+        if col % 2 == 0: 
+            for row in range(m_str-1, -1, -1): 
                 count += 1
                 bork[row][col] = count
         else:
@@ -17,6 +21,9 @@ def cols_is_even(m_str, m_col):
     return bork
 
 
+#   - Эта функция заполняет матрицу bork для нечетного количества столбцов аналогично функции cols_is_even.
+#   - Единственное отличие - условие на нечетные столбцы.
+#   - Результатом является заполненная матрица bork.
 def cols_is_uneven(m_str, m_col):
     global bork
     bork = [[0]*m_col for i in range(m_str)]
@@ -33,6 +40,8 @@ def cols_is_uneven(m_str, m_col):
     return bork
 
 
+#   - Эта функция в зависимости от четности/нечетности числа столбцов вызывает соответствующую функцию для заполнения матрицы.
+#   - Если количество столбцов четное, вызывается `colsisuneven`, иначе `colsiseven`.
 def matrix_build(m_str, m_col): 
     global bork
     if m_col % 2 == 0:
@@ -40,22 +49,23 @@ def matrix_build(m_str, m_col):
     else:
         cols_is_even(m_str, m_col)
 
-
+#   - Функция отражает матрицу по вертикали, меняя порядок строк на противоположный.
 def mat_mir_down(matrix_d):
     matrix_d = bork[::-1]
     return matrix_d
 
-
+#   - Данная функция отражает матрицу по горизонтали, меняя порядок элементов в каждой строке на обратный.
 def matr_mir_down_right(mirr_matrix):
-    matrix_d_r = [row[::-1] for row in mirr_matrix]
+    matrix_d_r = [row[::-1] for row in mirr_matrix] #срез, создание копии списка с элементами в обратном порядке
     return matrix_d_r
 
-
+#   - Эта функция выполняет двойное отражение матрицы: сначала по вертикали, а затем по горизонтали.
 def matr_mir_down_right_up(mirr_matrix_dru):
     matrix_d_r_u = mirr_matrix_dru[::-1]
     return matrix_d_r_u
 
-
+#   - Выводит на экран две матрицы: основную и ее отражение сначала вниз, затем вправо и вверх.
+#   - Выполняет сравнение построчно двух матриц и выводит результат с разделителями.
 def print_mirrored_matrix():
     print("основная + отзеркаленная вниз-вправо-вверх (вправо) матрицы")
     for item1, item2 in zip(bork, matr_mir_down_right_up(matr_mir_down_right(mat_mir_down(bork)))):
